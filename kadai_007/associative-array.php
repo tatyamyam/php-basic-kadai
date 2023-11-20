d4ff61c5df5ab459525d7aea3782dcb6d553aa77
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-11-20 16:38:33 +0900

    newupload_231120

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP連想配列の課題</title>
+</head>
+<body>
+  <p>
+    <?php 
+      //連想配列に値を代入
+      $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      //出力する
+      print_r($personal_data);
+    ?>
+  </p>
+</body>
+</html>
